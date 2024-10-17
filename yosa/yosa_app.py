7c23780a4a7932971b761a056824f7d9c88b214b
kennykatoma <kennykatoma@gmail.com>
2024-10-17 17:26:17 -0500

    Update yosa_app.py

--- yosa/yosa_app.py
+++ yosa/yosa_app.py
@@ -17,7 +17,7 @@ def toggle_theme():
     st.session_state.theme = 'dark' if st.session_state.theme == 'light' else 'light'
 
 # Theme toggle button
-st.button("Toggle Theme", on_click=toggle_theme)
+st.button("Toggle Backgrouns Color", on_click=toggle_theme)
 
 # Apply the theme
 if st.session_state.theme == 'dark':
