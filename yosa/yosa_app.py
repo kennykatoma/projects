7d3e586d1f0353de1fd80301a08877e34e0f826b
kennykatoma <kennykatoma@gmail.com>
2024-10-17 17:34:29 -0500

    Update yosa_app.py

--- yosa/yosa_app.py
+++ yosa/yosa_app.py
@@ -15,8 +15,10 @@ if 'theme' not in st.session_state:
 
 # Sidebar
 with st.sidebar:
-    # Display logo
-    st.image("yosa.png", width=200)  # Adjust width as needed
+    try:
+        st.image("yosa.png", width=200)  # Adjust width as needed
+    except Exception as e:
+        st.error(f"Error loading image: {e}")
     st.markdown("## Welcome!")
 
 # Hardcoded API keys
